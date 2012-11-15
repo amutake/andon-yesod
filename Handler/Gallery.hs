089a993d337d947b9265d0f941ddbe95e1c8535e
amkkun <amkkun@gmail.com>
2012-11-15 12:28:43 +0900

    put picture in top of gallery

--- Handler/Gallery.hs
+++ Handler/Gallery.hs
@@ -3,6 +3,7 @@ module Handler.Gallery where
 import Import
 import qualified Data.Map as M
 import Andon.Theme
+import Andon.Gallery
 
 getGalleryR :: Handler RepHtml
 getGalleryR = defaultLayout $ do
